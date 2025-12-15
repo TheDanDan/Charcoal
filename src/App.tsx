b5855976235dbaee13d43eb03825da84d52e5765
TheDanDan <77023480+TheDanDan@users.noreply.github.com>
2025-12-14 22:57:11 -0500

    feat: added basic landing page ui

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import './App.css'
+import FaultyTerminal from './components/FaultyTerminal'
+import WelcomeText from './components/WelcomeText'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-screen h-screen fixed inset-0 z-0 flex justify-center items-center'>
+        <FaultyTerminal
+          scale={4}
+          gridMul={[2, 1]}
+          digitSize={3}
+          timeScale={0.5}
+          pause={false}
+          scanlineIntensity={0.2}
+          glitchAmount={1}
+          flickerAmount={1}
+          noiseAmp={0.8}
+          chromaticAberration={0}
+          dither={0.2}
+          curvature={0.1}
+          tint="#a4a4a4"
+          mouseReact={true}
+          mouseStrength={0.5}
+          pageLoadAnimation={false}
+          brightness={0.75}
+        />
+      </div>
+
+      <div 
+        className='w-screen h-screen fixed inset-0 z-10 flex justify-center items-center' 
+        style={{ pointerEvents: 'none' }} 
+      >
+        <div style={{color: '#a4a4a4', fontFamily: 'monospace', fontSize: '1.5rem'}}> 
+          <WelcomeText />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
