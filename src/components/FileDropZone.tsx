a749c12eac7c963c5da7a4739358ba8308b2a46b
TheDanDan <77023480+TheDanDan@users.noreply.github.com>
2025-12-15 01:09:23 -0500

    feat: added file dropper

---
+++ src/components/FileDropZone.tsx
@@ -0,0 +1,37 @@
+import { Box, FileUpload, Icon } from "@chakra-ui/react"
+import { LuUpload } from "react-icons/lu"
+import DecryptedText from "./DecryptedText"
+
+const FileDropZone = () => {
+  return (
+    <FileUpload.Root maxW="xl" alignItems="stretch" maxFiles={1} accept={{'image/png': ['.png'],'image/jpeg': ['.jpg', '.jpeg']}}>
+      <FileUpload.HiddenInput />
+      <FileUpload.Dropzone>
+        <FileUpload.DropzoneContent>
+          <Box color="fg.muted">
+            {/* <Box>.png, .jpg up to 5MB</Box> */}
+            <Icon size="md" color="fg.muted">
+              <LuUpload />
+            </Icon>
+          </Box>
+          <Box>
+            <div style={{color: '#a4a4a4', fontFamily: 'monospace', fontSize: '1.5rem'}}> 
+              <DecryptedText
+                text="Drop a file to start"
+                animateOn="view"
+                speed={100}
+                maxIterations={30}
+              />
+            </div>
+          </Box>
+          {/* <Box color="fg.muted">
+            <Box>.png, .jpg up to 5MB</Box>
+          </Box> */}
+        </FileUpload.DropzoneContent>
+      </FileUpload.Dropzone>
+      {/* <FileUpload.List /> */}
+    </FileUpload.Root>
+  )
+}
+
+export default FileDropZone
